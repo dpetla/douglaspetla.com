70710e37a501f9be9405a903add1f2cf409b867b
Douglas Petla <dpetla@gmail.com>
2019-05-09 00:04:13 -0400

    blog module

---
+++ src/app/about/about.module.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AboutComponent } from './about.component';
+
+@NgModule({
+  declarations: [AboutComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: AboutComponent,
+        pathMatch: 'full'
+      }
+    ])
+  ]
+})
+export class AboutModule {}
